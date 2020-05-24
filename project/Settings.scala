64f0015475a07a51ede63ac283c505a7ed15a7f7
Michel Davit <michel.davit@gmail.com>
2020-05-24 15:04:24 +0200

    Fix settings for publishing

--- project/Settings.scala
+++ project/Settings.scala
@@ -10,7 +10,6 @@ object Settings {
       "-explaintypes", // Explain type errors in more detail.
       "-feature", // Emit warning and location for usages of features that should be imported explicitly.
       "-language:existentials", // Existential types (besides wildcard types) can be written and inferred
-      "-language:experimental.macros", // Allow macro definition (besides implementation and application)
       "-language:higherKinds", // Allow higher-kinded types
       "-language:implicitConversions", // Allow definition of implicit functions called views
       "-unchecked", // Enable additional warnings where generated code depends on assumptions.
