7367c84fa483a7da6ff4cc97849eac2556e5bbc7
Scala Steward <scala_steward@virtuslab.com>
2023-02-10 21:19:02 +0000

    Update fs2-core, fs2-io, fs2-scodec to 3.6.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,20 +1,16 @@
-import sbt._
+import sbt.*
 
-object Dependencies {
+object Dependencies:
 
-  object Versions {
-    val FS2      = "3.4.0"
+  object Versions:
+    val FS2      = "3.6.1"
     val MUnitCE3 = "1.0.7"
     val Scodec   = "2.2.0"
-  }
 
   val FS2Core    = "co.fs2"     %% "fs2-core"    % Versions.FS2
   val FS2IO      = "co.fs2"     %% "fs2-io"      % Versions.FS2
   val FS2Scodec  = "co.fs2"     %% "fs2-scodec"  % Versions.FS2
   val ScodecCore = "org.scodec" %% "scodec-core" % Versions.Scodec
 
-  object Test {
+  object Test:
     val MUnitCE3 = "org.typelevel" %% "munit-cats-effect-3" % Versions.MUnitCE3 % "it,test"
-  }
-
-}
