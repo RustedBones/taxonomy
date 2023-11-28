6087acd014ead82ffe0cb8c556c11063ddb3514a
Michel Davit <michel@davit.fr>
2023-11-28 17:47:58 +0100

    Update scodec to 2.2.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   object Versions {
     val FS2      = "3.4.0"
     val MUnitCE3 = "1.0.7"
-    val Scodec   = "2.2.0"
+    val Scodec   = "2.2.2"
   }
 
   val FS2Core    = "co.fs2"     %% "fs2-core"    % Versions.FS2
